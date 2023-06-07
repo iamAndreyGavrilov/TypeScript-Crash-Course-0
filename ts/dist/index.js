c40053199d8b164cc072c5f03bc7d29d78035f4d
AndreyG <boombardir20@gmail.com>
2023-06-07 13:56:00 +0500

    start 1

--- ts/dist/index.js
+++ ts/dist/index.js
@@ -1,2 +1,31 @@
 "use strict";
+// Basic Types
 let id = 5;
+let company = 'String Media';
+let isPublished = true;
+let x = 1;
+x = 'World';
+let age;
+age = 32;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Brad', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill'],
+];
+// Union
+let pinCode = '22';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Up);
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,1 +1,110 @@
+// Basic Types
 let id:number   = 5;
+let company:string = 'String Media';
+let isPublished:boolean = true;
+let x:any = 1;
+ x = 'World';
+let age:number;
+age = 32;
+
+let ids:number[] = [1,2,3,4,5];
+let arr:any[] = [1,true,'Hello'];
+
+// Tuple
+let person: [number,string,boolean] = [1,'Brad',true];
+// Tuple Array
+let employee: [number,string][];
+employee = [
+    [1,'Brad'],
+    [2,'John'],
+    [3,'Jill'],
+];
+
+// Union
+let pinCode: string | number = '22'
+
+// Enum
+enum Direction1 {
+    Up= 1,
+    Down,
+    Left,
+    Right
+}
+
+console.log(Direction1.Up)
+
+enum Direction2 {
+    Up= 'Up',
+    Down= 'Down',
+    Left= 'Left',
+    Right= 'Right'
+}
+
+// Objects
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: 'John'
+}
+
+// Type Assertion - это когда мы уверены в типе переменной
+let cid: any = 1;
+// let customerId = <number>cid
+let customerId = cid as number;
+
+// Functions
+function addNum(x:number,y:number): number {
+    return x + y;
+}
+addNum(1,2)
+
+// Void - это когда функция ничего не возвращает
+function log(message: string | number): void {
+    console.log(message)
+}
+
+// Interfaces - используется для объектов
+interface UserInterface {
+    readonly id: number, // только для чтения
+    name: string,
+    age?: number  // необязательный параметр
+}
+
+const user1: UserInterface = {
+    id: 1,
+    name: 'John'
+}
+
+// Interface with functions
+interface MathFunc {
+    (x:number,y:number): number
+}
+const add: MathFunc = (x:number,y:number): number => x + y;
+const sub: MathFunc = (x:number,y:number): number => x - y;
+
+
+// interface with classes
+interface PersonInterface {
+    id: number,
+    name: string,
+    register(): string
+}
+
+// Classes
+class Person implements PersonInterface {
+    id: number;
+    name: string;
+
+    constructor(id:number,name:string) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`
+    }
+}
+
